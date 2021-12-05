88b70734f946755fcbd1b9acc9726cfdee811561
AnuMI4 <anummirza@Anums-MacBook-Air.local>
2021-12-05 15:06:13 +0500

    added tests folder

--- tests/test-server.test.js
+++ tests/test-server.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('./index');
+const app = require('../index');
 describe('Sample Test', () => {
     it('should return correct message on /', async () => {
       const res = await request(app).get('/');
